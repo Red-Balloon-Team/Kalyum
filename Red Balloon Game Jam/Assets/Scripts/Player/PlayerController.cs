6494add1ee45e96a5cbaa5e5c23aaf81337e591a
guillemal <guillem.alqueza@estudiantat.upc.edu>
2023-06-16 17:24:40 +0200

    Player movement

---
+++ Red Balloon Game Jam/Assets/Scripts/Player/PlayerController.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField] private float moveSpeed = 1f;
+
+    private PlayerControls playerControls;
+    private Vector2 movement;
+    private Rigidbody2D rb;
+
+    private void Awake() {
+        playerControls = new PlayerControls();
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    private void OnEnable() {
+        playerControls.Enable();
+    }
+
+    // private void OnDisable() {
+    //     playerControls.Disable();
+    // }
+
+    private void Update() {
+        PlayerInput();
+    }
+
+    private void FixedUpdate() {
+        Move();
+    }
+
+    private void PlayerInput() {
+        movement = playerControls.Movement.Move.ReadValue<Vector2>();
+    }
+
+    private void Move() {
+        rb.MovePosition(rb.position + movement * moveSpeed * Time.fixedDeltaTime);
+    }
+}
